d775fb42f99848bffdeb7143181f51d8c8b8f065
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-09-15 00:22:32 +0200

    Adds Product entity

---
+++ classes/Entity/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Indigo Webshop package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webshop\Entity;
+
+use Doctrine\ORM\Mapping AS ORM;
+
+/**
+ * @ORM\Entity
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Product extends \Erp\Stock\Entity\Product
+{
+
+}
